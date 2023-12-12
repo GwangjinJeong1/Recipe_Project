e8d0c2932c1c75d5c43b366291e30aad30574c58
GwangjinJeong1 <dbsusqn12@gmail.com>
2023-12-12 20:24:42 +0900

    edit class

--- src/main/java/com/example/RecipeVO.java
+++ src/main/java/com/example/RecipeVO.java
@@ -4,25 +4,84 @@ import java.util.Date;
 
 public class RecipeVO {
 	private int seq;
-	private String title;
+	private String recipe_name;
 	private String writer;
-	private String content;
 	private String category;
+	private int recipe_level;
+	private String ingredient;
+	private String recipe_content;
+	private String image;
 	private Date regdate;
-	private int cnt;
-	
-	public int getSeq() {return seq;}
-	public void setSeq(int seq) {this.seq = seq;}
-	public String getTitle() {return title;}
-	public void setTitle(String title) {this.title = title;}
-	public String getWriter() {return writer;}
-	public void setWriter(String writer) {this.writer = writer;}
-	public String getContent() {return content;}
-	public void setContent(String content) {this.content = content;}
-	public String getCategory() {return category;}
-	public void setCategory(String category) {this.category = category;}
-	public Date getRegdate() {return regdate;}
-	public void setRegdate(Date regdate) {this.regdate = regdate;}
-	public int getCnt() {return cnt;}
-	public void setCnt(int cnt) {this.cnt = cnt;}
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getRecipe_name() {
+		return recipe_name;
+	}
+
+	public void setRecipe_name(String recipe_name) {
+		this.recipe_name = recipe_name;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getRecipe_level() {
+		return recipe_level;
+	}
+
+	public void setRecipe_level(int recipe_level) {
+		this.recipe_level = recipe_level;
+	}
+
+	public String getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(String ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	public String getRecipe_content() {
+		return recipe_content;
+	}
+
+	public void setRecipe_content(String recipe_content) {
+		this.recipe_content = recipe_content;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
 }
